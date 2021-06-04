ff330c1641b674d30f1c937484c22c98993eb577
neelapravalika <54702274+neelapravalika@users.noreply.github.com>
2021-06-04 12:47:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDGYBqQyTfH1wfvyuDp-Bs8PFWTMO6nwU8",
+    authDomain: "storytelling-b68ea.firebaseapp.com",
+    projectId: "storytelling-b68ea",
+    storageBucket: "storytelling-b68ea.appspot.com",
+    messagingSenderId: "745146925934",
+    appId: "1:745146925934:web:88fb0722e965adb289b77f"
+  };
+ 
